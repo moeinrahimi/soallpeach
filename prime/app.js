3f068ca0f7bb75d7a7b0782161e0b13caac4b68a
moein <imrrahimi@gmail.com>
2020-04-16 13:22:14 +0430

    stable

--- prime/app.js
+++ prime/app.js
@@ -1,25 +1,40 @@
 const fs = require('fs');
 let file = process.argv[2]
-const readline = require('readline');
-const readInterface = readline.createInterface({
-  input: fs.createReadStream(file)
-});
-function isPrime(n) {
-  if (n === 1) {
-    return false;
-  } else if (n === 2) {
-    return true;
-  } else {
-    for (var x = 2; x < n; x++) {
-      if (n % x === 0) {
-        return false;
-      }
+const outputPath = process.argv[3]
+let numbers = fs.readFileSync(file,{encoding:'utf-8'})
+
+function isPrime(num) {
+  var sqrtnum=Math.floor(Math.sqrt(num));
+    var prime = num != 1;
+    for(var i=2; i<sqrtnum+1; i++) { // sqrtnum+1
+        if(num % i == 0) {
+            prime = false;
+            break;
+        }
     }
-    return true;
-  }
+    return prime;
 }
-readInterface.on('line', function (number) {
-  if(isPrime(number)) console.log(`${number} is prime !`);
-});
 
+// const cache = []
+// console.time('start')
+let numbs = numbers.split('\n')
+let result = []
+numbs.forEach(number => {
+    if (isPrime(number)) {
+      result.push(number)
+    }
+})
+result=result.join('\n')
+if (outputPath) {
+  fs.writeFileSync(outputPath, result)
+} else {
+  process.stdout.write(result)
+}
+// console.timeEnd('start')
 
+// console.time('start')
+// console.log(isPrime(2147483647), 'first')
+// console.timeEnd('start')
+// console.time('start')
+// console.log(isPrime2(2147483647), 'second')
+// console.timeEnd('start')
