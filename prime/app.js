64e5689f451f4ce4b3cd5a78a51de256b6dd35a3
moein <imrrahimi@gmail.com>
2020-04-17 20:16:51 +0430

    new isPrime

--- prime/app.js
+++ prime/app.js
@@ -3,11 +3,10 @@ const fs = require('fs');
 let file = process.argv[2];
 const outputPath = process.argv[3];
 // 2.6 => 3.588
-// 2.6 => 3.588
+// 1.25 => ?
 
 let numbers = fs.readFileSync(file, { encoding: 'utf-8' });
-
-function isPrime(num) {
+function isPrimeORG(num) {
   var sqrtnum = Math.floor(Math.sqrt(num));
   var prime = num != 1;
   for (var i = 2; i < sqrtnum + 1; i++) {
@@ -20,6 +19,23 @@ function isPrime(num) {
   return prime;
 }
 
+function isPrime(number)
+{
+  if (number <= 3)
+  return true;
+
+  if (number%2 == 0 || number%3 == 0)
+  return false;
+
+  for (var i=5; i*i<=number; i=i+6)
+  {
+     if (number%i == 0 || number%(i+2) == 0)
+     return false;
+  }
+
+  return true;
+}
+
 function isPrime3 (n){
   return ![...Array(n).keys()]
     .slice(2)
@@ -28,25 +44,14 @@ function isPrime3 (n){
   }
 let numbs = numbers.split('\n');
 
-// let length = numbs.length;
-// let result = [];
-
-// for (let i = 0; i < length; i++) {
-//   let number = numbs[i];
-//   if (isPrime3(number)) {
-//     result.push(1);
-//   } else {
-//     console.log(number)
-//     result.push(0);
-//   }
-// }
+// console.time('start')
 let result = numbs.map(number => {
   if (isPrime(number))
     return 1
   return 0
 
 }).join('\n');
-
+// console.timeEnd('start')
 
 if (outputPath) {
   fs.writeFileSync(outputPath, result);
--- vlang/app.v
+++ vlang/app.v
@@ -1,11 +1,38 @@
-function isPrime(num) {
-  var sqrtnum=Math.floor(Math.sqrt(num));
-  var prime = num != 1;
-  for(var i=2; i<sqrtnum+1; i++) { // sqrtnum+1
-        if(num % i == 0) {
-            prime = false;
-            break;
-        }
+
+import math
+import os
+
+fn is_prime(num int) bool {
+	sqrtnum := math.floor(math.sqrt(num))
+	mut prime := num != 1
+	for i := 2; i < int(sqrtnum+1); i++ {
+		if(num % i == 0){
+			prime = false
+			break
+		}
+	}
+	return prime
+}
+
+
+fn main(){
+	input_file := os.args[1]
+	out_put_file := os.args[2]
+	mut result := []string
+	lines := os.read_lines(input_file) or {
+        panic('error reading file ')
     }
-    return prime;
+    for line in lines {
+			a := line.int()
+			if is_prime(a){
+	      result << "1"
+			}else{
+				result << "0"
+			}
+}
+	 mut file := os.create(out_put_file) or {
+		panic( err)
+	}
+	file.write(result.join('\n'))
+	file.close()
 }
