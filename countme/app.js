f90de068aec53fe6831b33ec84c0d394a51cd904
moein <imrrahimi@gmail.com>
2020-04-27 13:02:43 +0430

    docker build

--- countme/app.js
+++ countme/app.js
@@ -2,13 +2,27 @@ const fastify = require('fastify')({
   // logger: true
 })
 fastify.register(require('fastify-formbody'))
-
+// const rawBody = require('raw-body')
+// fastify.addContentTypeParser('*', (req, done) => {
+//   console.log(req.headers['content-length'])
+//   rawBody(req, {
+//     length: req.headers['content-length'],
+//     limit: '1mb',
+//     encoding: 'utf8', // Remove if you want a buffer
+//   }, (err, body) => {
+//   console.log("body", body)
+//     if (err) return done(err)
+//     done(null, parse(body))
+//   })
+// })
 let sum = 0
 
 fastify.post('/', function (req, reply) {
-  let number = Number(Object.keys(req.body)[0])
-  console.log("number", number)
-  sum += number
+  // console.log(req.body,'-------')
+  // let number = Number(Object.keys(req.body)[0])
+  // console.log("number", number)
+  sum += parseInt(req.body)
+  console.log("sum", sum)
   return reply.send(sum)
 })
 fastify.get('/count', function (req, reply) {
@@ -17,7 +31,7 @@ fastify.get('/count', function (req, reply) {
 })
 
 
-fastify.listen(80, function (err, address) {
+fastify.listen(80,'0.0.0.0', function (err, address) {
   if (err) {
     fastify.log.error(err)
     process.exit(1)
